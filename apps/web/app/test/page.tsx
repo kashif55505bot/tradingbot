d60e384ea120748a9b63125e9a9cd10a060523f0
kashif55505bot <kashif5555505@gmail.com>
2026-07-24 06:31:51 +0400

    Delete apps directory

--- apps/web/app/test/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Test() {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>✅ Test Page Working!</h1>
-      <p>If you see this, routing is working.</p>
-    </div>
-  );
-}
